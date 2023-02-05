6294229258d60e7db24c1abee6eaa0cd713240db
Mwxyz2022 <rozhukvlad@gmail.com>
2023-02-05 20:49:44 +0200

    solution

---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,30 @@
+import './index.scss'
+import store, { increment, decrement, reset } from './store.js'
+
+const resultElem = document.querySelector('.counter__result')
+const incrementBtn = document.querySelector('[data-action="increment"]')
+const resetBtn = document.querySelector('[data-action="reset"]')
+const decrementBtn = document.querySelector('[data-action="decrement"]')
+
+const onIncrement = () => {
+    store.dispatch(increment())
+}
+incrementBtn.addEventListener('click', onIncrement)
+
+const onDecrement = () => {
+    store.dispatch(decrement())
+}
+decrementBtn.addEventListener('click', onDecrement)
+
+const onReset = () => {
+    store.dispatch(reset())
+}
+resetBtn.addEventListener('click', onReset)
+
+store.subscribe(() => {
+    const state = store.getState()
+
+    const curentValue = state.history.reduce((acc, value) => acc + value, 0)
+    const historyString = state.history.map(value => (value === 1 ? `+${value}` : value)).join('')
+    resultElem.textContent = state.history.length === 0 ? '' : `${historyString}=${curentValue}`
+})
