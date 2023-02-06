b319edf888e4193dcc6ea23830cc06938c2d8f63
Mwxyz2022 <rozhukvlad@gmail.com>
2023-02-06 19:59:36 +0200

    fix

--- lesson3/hometask1/src/store.js
+++ lesson3/hometask1/src/store.js
@@ -1,8 +1,7 @@
 import { createStore, combineReducers } from 'redux'
-
-import languageReducer from './language.reducer'
-import cartReducer from './cart.reducer'
-import userReducer from './user.reducer'
+import { languageReducer } from './language.reducer'
+import { cartReducer } from './cart.reducer'
+import { userReducer } from './user.reducer'
 
 const appReducer = combineReducers({
     language: languageReducer,
@@ -14,4 +13,5 @@ const store = createStore(
     appReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
+
 export default store
