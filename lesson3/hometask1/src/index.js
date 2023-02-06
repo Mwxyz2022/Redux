a0856113896ff5d74e87deb2c45ffe5e4a2a4e58
Mwxyz2022 <rozhukvlad@gmail.com>
2023-02-06 20:01:31 +0200

    d

--- lesson3/hometask1/src/index.js
+++ lesson3/hometask1/src/index.js
@@ -1,10 +1,9 @@
 import store from './store'
-
-import setLanguage from './language.actions'
+import { setLanguage } from './language.actions'
 import { addProduct, removeProduct } from './cart.actions'
 import { setUser, removeUser } from './user.actions'
 
-///
+store.subscribe(() => console.log(store.getState()))
 store.dispatch(setLanguage('ua'))
 
 store.dispatch(addProduct({ id: 12, name: 'milk' }))
